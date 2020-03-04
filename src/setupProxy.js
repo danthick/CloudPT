570a19a262c815eb0ee52a8ea7f42ea4b52b5617
Dan Thick <dan.thick@hotmail.co.uk>
2020-03-04 16:26:33 +0000

    Changes to fetch functions to work with heroku deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy.createProxyMiddleware(["/api", , "/auth"], { target: "http://localhost:4000/" , changeOrigin: true}));
+    app.use(proxy.createProxyMiddleware(["/api", , "/auth"], { target: "https://cloudpt.me/" , changeOrigin: true}));
 };
