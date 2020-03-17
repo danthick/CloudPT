318b38ed11966f65c035e85cf392cd25590544fb
Dan Thick <dan.thick@hotmail.co.uk>
2020-03-17 12:55:19 +0000

    Registering service worker

--- src/index.js
+++ src/index.js
@@ -11,4 +11,14 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+//serviceWorker.register();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
