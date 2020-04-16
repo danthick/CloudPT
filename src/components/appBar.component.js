6312d1c3c3d6cec652f92baaee12c66e1f516374
Dan Thick <dan.thick@hotmail.co.uk>
2020-04-16 14:15:49 +0100

    Removing large white space below the app bar

--- src/components/appBar.component.js
+++ src/components/appBar.component.js
@@ -47,7 +47,7 @@ export default function ButtonAppBar(props) {
             </Typography>
             </Toolbar>
         </AppBar>
-        <br/><br/><br/><br/>
+        <br/><br/><p/>
         </div>
     );
 }
