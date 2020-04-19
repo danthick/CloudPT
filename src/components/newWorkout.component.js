a04d52bd670812c0bd069b3d0e9c0c819466795f
Dan Thick <dan.thick@hotmail.co.uk>
2020-04-19 21:16:46 +0100

    Carrying on implementation for creating a new workout

--- src/components/newWorkout.component.js
+++ src/components/newWorkout.component.js
@@ -8,30 +8,184 @@ export default class Workout extends Component{
 
         this.state = {
             exerciseType: ["Cardio", "Streching", "Body Weight", "Barbell", "Dumbbell"],
-            cardioExercises: ["Custom", "Treadmill", "Cross-Trainer", "Bike", "Rowing Machine", "Stair Climb", ""],
-            strechtingExercises: ["Custom", ],
-            bodyParts: ["Custom", "Legs", "Back", "Shoulders", "Chest", "Arms", "Abs", "Cardio"],
-            exercises: [["Custom", "Squat", "Front Squat", "Lunge"], // for legs
-                        ["Custom", "Deadlift"], // for back
-                        ["Custom"], // for shoulders
-                        ["Custom"], // for chest
-                        ["Custom"],
-                        ["Custom"],
-                        []],
-            bodyPart: 6,
+            exerciseTypeValue: -1,
+            bodyParts: ["Legs", "Back", "Shoulders", "Chest", "Arms", "Abs"],
+            bodyPartValue: -1,
+            showExerciseList: false,
+            showBodyPart: false,
+            exerciseValue: -1,
+            disableBttn: true,
+            exercise: [
+                        //Cardio
+                        [
+                            "Custom", "Treadmill", "Cross-Trainer", "Bike", "Rowing Machine", "Stair Climb", ""
+                        ],
+                        // Legs
+                        [
+                            // Streching
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ],
+                            // Body Weight
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ],
+                            // Barbell
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ],
+                            // Dumbbell
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ]
+                        ],
+                        // Back
+                        [
+                            // Streching
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ],
+                            // Body Weight
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ],
+                            // Barbell
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ],
+                            // Dumbbell
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ]
+                        ],
+                        // Shoulders
+                        [
+                            // Streching
+                            [
+                                "Custom", "Squat", "Front Squat", "Lunge"
+                            ],
+                            // Body Weight
+                            [
+
+                            ],
+                            // Barbell
+                            [
+
+                            ],
+                            // Dumbbell
+                            [
+
+                            ]
+                        ],
+                        // Chest
+                        [
+                            // Streching
+                            [
+
+                            ],
+                            // Body Weight
+                            [
+
+                            ],
+                            // Barbell
+                            [
+
+                            ],
+                            // Dumbbell
+                            [
+
+                            ]
+                        ],
+                        // Arms
+                        [
+                            // Streching
+                            [
+
+                            ],
+                            // Body Weight
+                            [
+
+                            ],
+                            // Barbell
+                            [
+
+                            ],
+                            // Dumbbell
+                            [
+
+                            ]
+                        ],
+                        // Abs
+                        [
+                            // Streching
+                            [
+
+                            ],
+                            // Body Weight
+                            [
+
+                            ],
+                            // Barbell
+                            [
+
+                            ],
+                            // Dumbbell
+                            [
+
+                            ]
+                        ],
+                    ],
         }
     }
 
-    addExercise(){
-
-
+    exerciseTypeChange(e){
+        this.setState({
+            showBodyPart: false,
+            showExerciseList: false,
+        })
+        this.setState({exerciseTypeValue: e.target.value}); 
+        if (e.target.value === "0"){
+            this.setState({showExerciseList: true})
+        } else if (e.target.value > 0){
+            this.setState({showBodyPart: true})
+        }
     }
+
+    bodyPartChange(e){
+        console.log(e.target.value)
+        this.setState({bodyPartValue: e.target.value})
+        if(e.target.value > -1){
+            this.setState({showExerciseList: true})
+        }
+    }
+
+    exerciseChange(e){
+        this.setState({exerciseValue: e.target.value})
+        console.log("SHOW TIME")
+        console.log(this.state.exerciseTypeValue)
+        // Exercise is cardio - show time/distance
+        if(this.state.exerciseTypeValue === "0"){
+            console.log("SHOW TIME")
+        // Exercise is body weight - no need to show weight
+        } else if (0 < this.state.exerciseTypeValue < 2) {
+        
+        // Exercise is weighted - show weight
+        } else {
+
+        }
+    }
+
+    addExercise(){}
+
+    
+
+
 
 
     render() {
         return (
             <Fragment>
-                <AppBar width="100%" pageName="NEW WORKOUT" back="/workout"/>
+                <AppBar width="100%" pageName="NEW WORKOUT" back="/workout"/><br/>
 
                 <form onSubmit={this.addWorkout}>
                     <input  
@@ -44,9 +198,12 @@ export default class Workout extends Component{
                         style={{textAlign: "center"}}
                     /><br/>
 
-                    <button type="button" className="btn btn-primary container" data-toggle="modal" data-target="#addExercise">Add Exercise</button><br/><br/>
+                    <button type="button" className="btn btn-success container" data-toggle="modal" data-target="#addExercise">ADD EXERCISE</button><br/><br/>
 
                     <div className="bubbleCard">Will be a list of exercises in workout</div><br/>
+
+                    <button type="button" className="btn btn-primary container" data-toggle="modal" data-target="#addExercise">CREATE WORKOUT</button>
+
                 </form>
 
 
@@ -63,32 +220,43 @@ export default class Workout extends Component{
                                 <button type="button" className="close" data-dismiss="modal">&times;</button>
                             </div>
                             <div className="modal-body">
+                            
                                 Exercise Type
-                                <select className="form-control" onChange={(e) => this.setState({bodyPart: e.target.value})}>
+                                <select className="form-control" onChange={(e) => this.exerciseTypeChange(e)}>
                                     <option hidden >Choose exercise type...</option>
                                     {this.state.exerciseType.map((exerciseType, index) => <option value={index} key={index}>{exerciseType}</option>)}
                                 </select><br/>
 
-                                Body Part
-                                <select className="form-control" onChange={(e) => this.setState({bodyPart: e.target.value})}>
-                                    <option hidden >Choose a body part...</option>
-                                    {this.state.bodyParts.map((bodyPart, index) => <option value={index} key={index}>{bodyPart}</option>)}
-                                </select><br/>
+                                {this.state.showBodyPart?
+                                <div>
+                                    Body Part
+                                    <select className="form-control" onChange={(e) => this.bodyPartChange(e)}>
+                                        <option hidden >Choose a body part...</option>
+                                        {this.state.bodyParts.map((bodyPart, index) => <option value={index} key={index}>{bodyPart}</option>)}
+                                    </select><br/>
+                                </div>
+                                : null }
 
+
+                                {this.state.showExerciseList?
+                                <div>
                                 Exercise
-                                <select className="form-control" onChange={(e) => this.setState({exercise: e.target.value})}>
+                                <select required className="form-control" onChange={(e) => this.exerciseChange(e) }>
                                     <option hidden >Choose a exercise...</option>
-                                    {this.state.exercises[this.state.bodyPart].map((exercise, index) => <option value={index} key={index}>{exercise}</option>)}
-                                </select>
-                                {this.state.exercise < 1 &&
-                                    <input type="text" className="form-control" placeholder="Exercise Name" value={this.state.firstName} onChange={this.onChangeFirstName} required/>
+                                    {this.state.exercise[this.state.exerciseTypeValue].map((exercise, index) => <option value={index} key={index}>{exercise}</option>)}
+                                </select><br/>
+                                {this.state.exerciseValue === "0" &&
+                                    <input required type="text" className="form-control" placeholder="Exercise Name" value={this.state.firstName} onChange={this.onChangeFirstName}/>
                                 }
+                                </div>
+                                : null}
+                            
                             </div>
 
 
                             <div className="modal-footer">
-                                {this.state.exercise}
-                                <button type="button" className="btn btn-primary"  data-dismiss="modal" onClick={this.addExercise}>Add</button>
+                                {this.state.exerciseValue}
+                                <button type="submit" className="btn btn-primary" disabled={this.state.disableBttn}>Add</button>
                                 <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
                             </div>
                         </div>
