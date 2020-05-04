dccf1ea928b27cae92837b6a73791d402f7debf9
Dan Thick <dan.thick@hotmail.co.uk>
2020-05-04 15:57:35 +0100

    Adding backend unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './components/account/account.component';
 
+
 describe('First React component test with Enzyme', () => {
    it('renders without crashing', () => {
       shallow(<App />);
