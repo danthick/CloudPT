ca3e444b8c481d888fcd002ad77ac67b9b21739d
Dan Thick <dan.thick@hotmail.co.uk>
2020-05-05 15:08:03 +0100

    Adding more exercise options

--- src/data/exercises.js
+++ src/data/exercises.js
@@ -4,68 +4,68 @@ const exercise = [
     // Legs
     [
         // Streching
-        ["Custom", "leg stretch", "Front Squat", "Lunge"],
+        ["Custom", "Quad Strech", "Hamstring Stretch", "Calf Stretch"],
         // Body Weight
-        ["Custom", "Squat", "Front Squat", "Lunge"],
+        ["Custom", "Squat", "Squat Jump", "Lunge", "Box Squats", "Wall Sit"],
         // Barbell
-        ["Custom", "Squat", "Front Squat", "Lunge"],
+        ["Custom", "Squat", "Front Squat", "Romanian Deadlift", "Lunges", "Good Mornings", "Calf Raises", "Glute Bridge"],
         // Dumbbell
-        ["Custom", "Squat", "Front Squat", "Lunge"]
+        ["Custom", "Goblet Squat", "Bulgarian Squat", "Romanian Deadlift", "Lunges"]
     ],
     // Back
     [
         // Streching
-        ["Custom", "back stretch", "Front Squat", "Lunge"],
+        ["Custom", "Childs Pose", "Knee-to-Chest", "Seated Spinal Twist", "Cat and Cow Stretch"],
         // Body Weight
-        ["Custom", "Squat", "Front Squat", "Lunge"],
+        ["Custom", "Pullups", "Chin Ups", "Inverted Rows"],
         // Barbell
-        ["Custom", "Squat", "Front Squat", "Lunge"],
+        ["Custom", "Deadlift", "Bent Over Rows", "Stiff Leg Deadlift", "Rack Pulls", "Shrugs"],
         // Dumbbell
-        ["Custom", "Squat", "Front Squat", "Lunge"]
+        ["Custom", "Bent Over Rows", "Kneeling One Arm Row", "Deadlift", "Back Fly"]
     ],
     // Shoulders
     [
         // Streching
-        ["Custom", "Squat", "Front Squat", "Lunge"],
+        ["Custom", "Childs Pose", "Arm Circles", "Arm Cross-Body", "Downward Dog"],
         // Body Weight
-        [],
+        ["Custom", "Pike Pushups", "Push-Back Pushups"],
         // Barbell
-        [],
+        ["Custom", "Shoulder Press", "Upright Row", "Seated Shoulder Press"],
         // Dumbbell
-        []
+        ["Custom", "Shoudler Press", "Arnold Press", "Lateral Raise", "Reverse Flys"]
     ],
     // Chest
     [
         // Streching
-        [],
+        ["Custom", "Bent Arm Against Wall", "Hands on Head"],
         // Body Weight
-        [],
+        ["Custom", "Pushups", "Diamond Pushups", "Wide Pushups", "Incline Pushups", "Decline Pushups"],
         // Barbell
-        [],
+        ["Custom", "Bench Press", "Incline Bench Press", "Decline Bench Press", "Close-Grip Bench Press"],
         // Dumbbell
-        []
+        ["Custom", "Bench Press", "Incline Bench Press", "Decline Bench Press", "Lying Flys", "Incline Flys"]
     ],
     // Arms
     [
         // Streching
-        [],
+        ["Custom", "Overhead Tricep", "Arm Cross-Body", "Wrist Extension", "Childs Pose"],
         // Body Weight
-        [],
+        ["Custom", "Tricep Dips", "Shoulder Taps", "Walking Pushups", "Diamond Pushups"],
         // Barbell
-        [],
+        ["Custom", "Curls", "Skull Crushes", "Reverse Curls"],
         // Dumbbell
-        []
+        ["Custom", "Curls", "Hammer Curls", "Overhead Extensions", "Bent-Over Tricep Extensions" ]
     ],
     // Abs
     [
         // Streching
-        [],
+        ["Custom", "Snake Strech", "Childs Pose", "Cat and Cow Stretch"],
         // Body Weight
-        [],
+        ["Custom", "Crunch", "Russian Twists", "Plank", "Side-Plank", "Leg Raises", "bicycles", "Side Oblique Crunch"],
         // Barbell
-        [],
+        ["Custom", "Barbell Rollout", "Zercher Squat", "Lunge"],
         // Dumbbell
-        []
+        ["Custom", "Side Bends", "Crunch", "Russian Twists"]
     ],
 ]
 
