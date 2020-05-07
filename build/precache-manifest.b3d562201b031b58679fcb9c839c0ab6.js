f091f200b286133e59664e266269008a285d4ea5
Dan Thick <dan.thick@hotmail.co.uk>
2020-05-07 10:35:16 +0100

    Testing heroku production build

---
+++ build/precache-manifest.b3d562201b031b58679fcb9c839c0ab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13922dd6cc2123a731d92a8ea95d25a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7656d021ba10096ad97a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "09e9c78b97a19f22b72e",
+    "url": "/static/css/main.b8b7e809.chunk.css"
+  },
+  {
+    "revision": "7656d021ba10096ad97a",
+    "url": "/static/js/2.7000090b.chunk.js"
+  },
+  {
+    "revision": "c9d05805b18675df5511f84315bc8683",
+    "url": "/static/js/2.7000090b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e9c78b97a19f22b72e",
+    "url": "/static/js/main.963d9c4f.chunk.js"
+  },
+  {
+    "revision": "ccdb3282956613eeca36",
+    "url": "/static/js/runtime-main.b1e9b902.js"
+  }
+]);
